5a9b3ab071d9ee130a90a6b7c1b9afdd86ca7b44
Carlos Jaime <jaime_andrek@hotmail.com>
2022-07-08 18:32:46 -0300

    DockBlock update

--- app/Controllers/User/Index.php
+++ app/Controllers/User/Index.php
@@ -8,12 +8,21 @@ use Src\help\Json;
 class Index
 {
 
+    /**
+     * @param Post $post
+     * @param Json $json
+     */
     public function __construct(Post $post, Json $json)
     {
         $this->post = $post;
         $this->json = $json; 
     }
 
+    /**
+     * get http request
+     *
+     * @return void
+     */
     public function index()
     {
         $httpMethod = strtolower($_SERVER['REQUEST_METHOD']);
